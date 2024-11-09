e996638b647879f07df158d1c0b4a0520f287613
Benard Kaniaru <kaniarubenard623@gmailcom>
2024-11-09 19:24:29 +0300

    apply inline CSS styling to React components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p style={{ fontWeight: "bold" }}>
+        &copy; <span style={{ color: "red" }}>2023 City Lovers</span>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <main>
+      <p style={{ color: "navy-blue", fontSize: "25px" }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
