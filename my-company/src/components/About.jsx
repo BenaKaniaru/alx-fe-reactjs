1653b2cda7690afa608ba81a134175b99ab04574
Benard Kaniaru <kaniarubenard623@gmailcom>
2024-11-10 23:49:25 +0300

    build simple company web with React

---
+++ my-company/src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Our company has been providing top-notch services since 1990. We
+        specialize in various fields including technology, marketing, and
+        consultancy
+      </p>
+    </div>
+  );
+}
---
+++ my-company/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Our Company</h1>
+      <p>We are dedicated to delivering excellence in all our services</p>
+    </div>
+  );
+}
