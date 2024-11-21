bca0fb4d8abdab50389e2f221740367dd61b5c8a
Benard Kaniaru <kaniarubenard623@gmailcom>
2024-11-21 18:17:15 +0300

    fix implementation of the recipe store

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,6 +4,7 @@ const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (newRecipes) => ({ recipes: newRecipes }),
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
