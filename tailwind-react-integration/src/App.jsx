1aff11e5da83bcb95b7d85014dc4cf36ef49dafe
Benard Kaniaru <kaniarubenard623@gmailcom>
2024-12-05 11:58:03 +0300

    add and style a user profile component

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,6 +1,11 @@
+import UserProfile from "./components/UserProfile";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-red-50">Hello world!</h1>
+    <div>
+      <h1 className="text-3xl font-bold underline bg-red-50">Hello world!</h1>
+      <UserProfile />
+    </div>
   );
 }
 
