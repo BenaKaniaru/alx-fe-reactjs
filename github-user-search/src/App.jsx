06475c37c4102536008a994971d7d4b2ea548c8c
Benard Kaniaru <kaniarubenard623@gmailcom>
2024-12-22 22:43:28 +0300

    complete project

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,6 +2,7 @@ import Search from "./components/search";
 export default function App() {
   return (
     <div>
+      <h1>Github User Search</h1>
       <Search />
     </div>
   );
--- github-user-search/src/services/githubServices.js
+++ github-user-search/src/services/githubServices.js
@@ -1,3 +1,12 @@
 import axios from "axios";
 
-const GITHUB_API_URL = "https://api.github.com";
+const BASE_URL = "https://api.github.com/users";
+
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/${username}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
