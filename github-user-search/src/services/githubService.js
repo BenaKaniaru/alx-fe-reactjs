abb1819409eba314d96f890bd35e620904d5d802
Benard Kaniaru <kaniarubenard623@gmailcom>
2024-12-22 23:28:33 +0300

    fix mapping

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,30 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/users";
+const BASE_URL = "https://api.github.com/search/users?q";
 
-export const fetchUserData = async (username) => {
+export const fetchAdvancedSearch = async ({ username, location, minRepos }) => {
   try {
-    const response = await axios.get(`${BASE_URL}/${username}`);
+    // Construct the query string
+    let query = username ? `${username}` : "";
+
+    if (location) {
+      query += `+location:${location}`;
+    }
+
+    if (minRepos) {
+      query += `+repos:>${minRepos}`;
+    }
+
+    // Make the API request
+    const response = await axios.get(`${BASE_URL}${query}`, {
+      headers: {
+        Accept: "application/vnd.github.v3+json",
+      },
+    });
+
     return response.data;
   } catch (error) {
+    console.error("Error fetching data from GitHub API:", error);
     throw error;
   }
 };
